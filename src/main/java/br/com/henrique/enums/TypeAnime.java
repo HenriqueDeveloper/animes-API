8278b1b8f94288f4e7d358d4e4d413058639befe
Carlos Henrique <carlossoaressantana@hotmail.com>
2023-07-05 14:15:28 -0300

    feat: adicionar enums

---
+++ src/main/java/br/com/henrique/enums/TypeAnime.java
@@ -0,0 +1,7 @@
+package br.com.henrique.enums;
+
+public enum TypeAnime {
+	
+	TV, MOVIE, SPECIAL, OVA, ONA, MUSIC;
+
+}
---
+++ src/main/java/br/com/henrique/enums/TypeOrderAnime.java
@@ -0,0 +1,8 @@
+package br.com.henrique.enums;
+
+public enum TypeOrderAnime {
+
+MAL_ID, TITLE, TYPE, RATING, START_DATE, END_DATE, EPISODES, SCORE, SCORE_BY, RANK, POPULARITY, MEMBERS, FAVORITE;
+
+
+}
