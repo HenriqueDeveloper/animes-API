570a5cd5c2ffd22788921432627f40d5a55557e9
Carlos Henrique <carlossoaressantana@hotmail.com>
2023-07-05 14:24:55 -0300

    feat: adicionar configuração do swagger

---
+++ src/main/java/br/com/henrique/configurations/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package br.com.henrique.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "animes-API", version = "1.0",
+        contact = @Contact(name = "Carlos Henrique")),
+        security = {@SecurityRequirement(name = "bearerToken")}
+)
+@SecuritySchemes({
+        @SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP,
+                scheme = "bearer", bearerFormat = "JWT")
+})
+public class SwaggerConfig {
+
+}
+
+
